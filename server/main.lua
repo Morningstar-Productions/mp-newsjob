e3300ce130b645fb75540876fbac69f12c483a9e
xViperAG <82969741+xViperAG@users.noreply.github.com>
2022-08-21 17:30:22 -0400

    Commented Out Commands

--- server/main.lua
+++ server/main.lua
@@ -1,6 +1,6 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-QBCore.Commands.Add("newscam", "Grab a news camera", {}, false, function(source, _)
+--[[QBCore.Commands.Add("newscam", "Grab a news camera", {}, false, function(source, _)
     local Player = QBCore.Functions.GetPlayer(source)
     if Player.PlayerData.job.name == "reporter" then
         TriggerClientEvent("Cam:ToggleCam", source)
@@ -19,4 +19,22 @@ QBCore.Commands.Add("newsbmic", "Grab a Boom microphone", {}, false, function(so
     if Player.PlayerData.job.name == "reporter" then
         TriggerClientEvent("Mic:ToggleBMic", source)
     end
+end)]]--
+
+QBCore.Functions.CreateUseableItem("newsbmic", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("Mic:ToggleBMic", source)
+end)
+
+QBCore.Functions.CreateUseableItem("newscam", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("Cam:ToggleCam", source)
+end)
+
+QBCore.Functions.CreateUseableItem("newsmic", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("Mic:ToggleMic", source)
 end)

490c72a505ab57e287ef544bae0e8d5b744772d0
xViperAG <usairsoftingandgaming@gmail.com>
2023-04-27 15:48:39 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,10 @@
 fx_version 'cerulean'
 game 'gta5'
+lua54 'yes'
 
-description 'mp-newsjob'
+name 'mp-newsjob'
+description 'Enhanced News Job for QBCore'
+author 'xViperAG'
 version '2.0.0'
 
 shared_scripts {
@@ -19,7 +22,6 @@ server_script 'server/main.lua'
 dependencies {
     'futte-newspaper',
     'ox_lib',
-    'ox_target'
+    'qb-target',
+    'ox_inventory'
 }
-
-lua54 'yes'
